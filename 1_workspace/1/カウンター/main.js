2feb62eb41899ced9d9d19027d84253995a13c56
guchi0 <sp9g83t7@gmail.com>
2021-04-17 02:55:29 +0900

    レシピの追加

---
+++ 1_workspace/1/カウンター/main.js
@@ -0,0 +1,9 @@
+const display=document.getElementById("display")
+const plusbutton=document.getElementById("plus-button")
+let count=0
+
+
+plusbutton.onclick=function(){ 
+    count=count +1 
+    display.textContent =count
+}
